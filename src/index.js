0bca48e893e0bfd355a8da3102d989f5e490c67f
Barbara Kogus <barbarakogus@gmail.com>
2022-02-23 16:46:22 +0100

    search input is saved in the localStorage

--- src/index.js
+++ src/index.js
@@ -1,21 +1,36 @@
 import aunty from './images/aunty.png'
 import './styles/main.scss'
 
-//   // Create paragraph node
-//   const p = document.createElement('p')
-//   p.textContent = `I like ${myGame.name}.`
-// // Create heading node
-const search = document.createElement('INPUT')
-search.setAttribute("type", "text");
-const submitbtn = document.createElement('BUTTON')
+const search = document.createElement('INPUT');
+search.setAttribute("list", "dataList");
+
+const dataList = document.createElement('datalist');
+dataList.setAttribute("id", "dataList");
+
+const option = document.createElement('option');
+option.setAttribute("value", "chocolate");
+dataList.appendChild(option);
+
+search.appendChild(dataList);
+
+const submitbtn = document.createElement('BUTTON');
 submitbtn.textContent = "search"
 
+const searchHistory = document.createElement('P');
 
+const searchList = JSON.parse(localStorage.getItem('search'));
 
+submitbtn.addEventListener('click', () => {
+    const value = search.value;
+    searchList.push(value);
+    localStorage.setItem('search', JSON.stringify(searchList));
+});
 
-
+const items =  localStorage.getItem('search');
+console.log(items);
 
 // // Append heading node to the DOM
 const app = document.querySelector('#root')
-app.append(search)
-app.append(submitbtn)
+app.append(search);
+app.append(submitbtn);
+app.append(searchHistory);
