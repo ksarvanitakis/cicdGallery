aff04b4fae5559c2ab0967339b71d38ebe5a2139
SumanaSattar <sumana.sattar@gmail.com>
2022-02-24 10:52:34 +0100

    serch bar working

--- src/api.js
+++ src/api.js
@@ -1,8 +1,16 @@
 
-const getPhotosFromUnsplash = () => {
-    const photos = fetch("https://api.unsplash.com/search/photos?query=london&client_id=hraxE2dYfvMDF9u9uvl2eGW6s1zDW2JVWUMcB-83khQ")
+const getPhotosFromUnsplash = (value) => {
+    const photos = fetch(`https://api.unsplash.com/search/photos?query=${value}&client_id=hraxE2dYfvMDF9u9uvl2eGW6s1zDW2JVWUMcB-83khQ`)
         .then(response => response.json())
-        .then(txt => console.log(txt))
-}
+        .then(data => loadImages(data))
 
+}
+const loadImages = (data) => {
+    for(let i = 0;i < data.results.length;i++){
+      let image = document.createElement("div");
+      image.className = "img";
+      image.style.backgroundImage = "url("+data.results[i].urls.raw + "&w=1366&h=768" +")";
+      document.querySelector(".image-container").appendChild(image);
+    }
+  }
 module.exports = getPhotosFromUnsplash;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@
 import './styles/main.scss';
 import getPhotos from './api.js';
 
-getPhotos();
-
-const searchList = JSON.parse(localStorage.getItem('search')) || [];
+/////Search///////////
+const searchList = JSON.parse(localStorage.getItem('search')) || [];
 
 const search = document.createElement('INPUT');
 search.setAttribute("list", "dataList");
@@ -16,37 +15,46 @@ const renderList = () => {
     searchList.forEach(element => {
         const option = document.createElement('option');
         option.setAttribute("value", element);
-        dataList.appendChild(option); 
+        dataList.appendChild(option);
     });
 }
+const items = localStorage.getItem('search');
+console.log(items);
 
+renderList()
+search.appendChild(dataList);
 
+/////////////button//////////
 const submitbtn = document.createElement('BUTTON');
 submitbtn.textContent = "search"
 
 const searchHistory = document.createElement('P');
 
 submitbtn.addEventListener('click', () => {
+    //imageContainer.removeChild('img');
+   
+    while (imageContainer.firstChild) {
+        imageContainer.firstChild.remove()
+    }
     const value = search.value;
     searchList.push(value);
     localStorage.setItem('search', JSON.stringify(searchList));
-    
+
     const option = document.createElement('option');
     option.setAttribute("value", value);
-    dataList.appendChild(option); 
+    dataList.appendChild(option);
+    getPhotos(value);
 });
-
-const items =  localStorage.getItem('search');
-console.log(items);
-
-renderList()
-search.appendChild(dataList);
+//////////div/////
+const imageContainer = document.createElement('div')
+imageContainer.className = "image-container"
 
 // // Append heading node to the DOM
 const app = document.querySelector('#root')
 app.append(search);
 app.append(submitbtn);
 app.append(searchHistory);
+app.append(imageContainer);
 
 
 
